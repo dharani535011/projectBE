7aed735d44dc74393e068c5b605676927b5cd431
dharani535011 <dharani535011@gmail.com>
2024-07-29 12:36:49 +0530

    commit

--- index.js
+++ index.js
@@ -1,139 +1,3 @@
-// const express = require("express");
-// const app = express();
-// const cors = require("cors");
-// const path = require("path");
-// const fs = require("fs");
-// const cookieParser = require('cookie-parser');
-// const mongoose = require("mongoose");
-// const multer = require("multer");
-// const { MONGO, PORT, STRIPE_SECRET_KEY } = require("./config");
-// const UserRouter = require("./Routers/UsersRouter");
-// const User = require("./Models/UsersModel");
-// const ServiceRouter = require("./Routers/ServiceRouter");
-// const moment=require("moment")
-// const Stripe = require('stripe')
-// // require('dotenv').config();
-
-// const stripe = Stripe(STRIPE_SECRET_KEY)
-
-// app.use(cookieParser());
-// app.use(express.json());
-// app.use('/uploads', express.static(path.join(__dirname, "uploads")))
-// app.use(cors({
-//     origin: (origin, callback) => {
-//         callback(null, true);
-//     },
-//     credentials: true
-// }));
-// app.use("/user", UserRouter);
-// app.use("/service", ServiceRouter);
-
-// // Photo upload code
-// // const fileSchema = new mongoose.Schema({
-// //     filename: String,
-// //     path: String,
-// //     contentType: String
-// // });
-
-// // const File = mongoose.model("File", fileSchema, "photos");
-
-// const storage = multer.diskStorage({
-//     destination: (req, file, cb) => {
-//         cb(null, "uploads/");
-//     },
-//     filename: (req, file, cb) => {
-//         cb(null, Date.now() + path.extname(file.originalname));
-//     }
-// });
-
-// const upload = multer({ storage });
-
-// const uploadDir = "./uploads";
-// if (!fs.existsSync(uploadDir)) {
-//     fs.mkdirSync(uploadDir);
-// }
-
-// app.post("/upload", upload.single("file"), async (req, res) => {
-//     // const file = new File({
-//     //     filename: req.file.filename,
-//     //     path: req.file.path,
-//     //     contentType: req.file.mimetype
-//     // });
-//     const { id } = req.body;
-//     try {
-//         if (!id) {
-//             return res.status(400).send({ message: "Id required" });
-//         }
-
-//         const user = await User.findById(id);
-//         if (!user) {
-//             return res.status(404).send({ message: "User not found" });
-//         }
-
-//         // Delete the previous image if it exists
-//         if (user.image && fs.existsSync(user.image)) {
-//             fs.unlink(user.image, (err) => {
-//                 if (err) {
-//                     console.error("Error deleting the file:", err);
-//                     return res.status(500).send({ message: "Error deleting the previous file" });
-//                 }
-//             });
-//         }
-
-//         // Save the new image path
-//         user.image = req.file.path;
-//         await user.save();
-
-//         res.send({ message: "Photo uploaded" });
-//     } catch (error) {
-//         res.status(500).send({ message: error.message });
-//     }
-// });
-
-// // payment
-
-// app.post('/create-payment-intent', async (req, res) => {
-//     const { amount } = req.body;
-
-//     try {
-//         const paymentIntent = await stripe.paymentIntents.create({
-//             amount: amount,
-//             currency: 'inr',
-//         });
-
-//         res.status(200).send({
-//             clientSecret: paymentIntent.client_secret,
-//         });
-//     } catch (error) {
-//         res.status(500).send({
-//             error: error.message,
-//         });
-//     }
-// })
-
-
-
-// // 
-
-
-
-
-
-
-
-
-// console.log(moment().toDate())
-// const port = 3000 || PORT;
-// mongoose.connect(MONGO)
-//     .then(() => {
-//         console.log("MongoDB connected");
-//         app.listen(port, () => {
-//             console.log(`Server running on port ${port}`);
-//         });
-//     })
-//     .catch(err => {
-//         console.error("MongoDB connection error:", err);
-//     });
 
 
 
@@ -152,24 +16,14 @@ const User = require("./Models/UsersModel");
 const ServiceRouter = require("./Routers/ServiceRouter");
 const moment = require("moment");
 const Stripe = require('stripe');
-// require('dotenv').config();
+
 
 const stripe = Stripe(STRIPE_SECRET_KEY);
 
 app.use(cookieParser());
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, "uploads")));
-// app.use(cors({
-//     origin: (origin, callback) => {
-//         callback(null, true); // Allow all origins
-//     },
-//     credentials: true
-// }));
-// app.use(cors({
-//     origin: '*', // Allow all origins
-//     credentials: true
-// }));
-// const allowedDomains = ['https://stupendous-taffy-488b03.netlify.app/', 'http://localhost:5173'];
+
 
 const allowedDomains = ['https://stupendous-taffy-488b03.netlify.app', 'http://localhost:5173',"https://incomparable-sherbet-461c12.netlify.app","https://magical-kheer-0ce23a.netlify.app"];
 
@@ -259,7 +113,7 @@ app.post('/create-payment-intent', async (req, res) => {
 });
 
 console.log(moment().toDate());
-const port = 3000 || PORT;
+const port = 3000;
 mongoose.connect(MONGO)
     .then(() => {
         console.log("MongoDB connected");
